90a23a39ccf3be916b8ea83daeb4dde48d85037d
Anthony Medina <anthony.akmp99@gmail.com>
2026-04-23 16:03:46 +0000

    Se agrego propiedades de editor para asignar mandante para apis

--- zpluginPutBatchWCTA/webapp/zpluginPutBatchWCTA/builder/PropertyEditor.js
+++ zpluginPutBatchWCTA/webapp/zpluginPutBatchWCTA/builder/PropertyEditor.js
@@ -1,36 +1,36 @@
 sap.ui.define([
-    "sap/ui/model/resource/ResourceModel",
-    "sap/dm/dme/podfoundation/control/PropertyEditor"
+	"sap/ui/model/resource/ResourceModel",
+	"sap/dm/dme/podfoundation/control/PropertyEditor"
 ], function (ResourceModel, PropertyEditor) {
-    "use strict";
-    
-    var oFormContainer;
+	"use strict";
 
-    return PropertyEditor.extend( "serviacero.custom.plugins.zpluginPutBatchWCTA.zpluginPutBatchWCTA.builder.PropertyEditor" ,{
+	var oFormContainer;
 
-		constructor: function(sId, mSettings){
+	return PropertyEditor.extend("serviacero.custom.plugins.zpluginPutBatchWCTA.zpluginPutBatchWCTA.builder.PropertyEditor", {
+
+		constructor: function (sId, mSettings) {
 			PropertyEditor.apply(this, arguments);
-			
+
 			this.setI18nKeyPrefix("customComponentListConfig.");
 			this.setResourceBundleName("serviacero.custom.plugins.zpluginPutBatchWCTA.zpluginPutBatchWCTA.i18n.builder");
 			this.setPluginResourceBundleName("serviacero.custom.plugins.zpluginPutBatchWCTA.zpluginPutBatchWCTA.i18n.i18n");
 		},
-		
-		addPropertyEditorContent: function(oPropertyFormContainer){
-			var oData = this.getPropertyData();
-						
-			this.addInputField(oPropertyFormContainer, "title", oData);
-			this.addInputField(oPropertyFormContainer, "text", oData);
 
-            oFormContainer = oPropertyFormContainer;
+		addPropertyEditorContent: function (oPropertyFormContainer) {
+			var oData = this.getPropertyData();
+
+			this.addInputField(oPropertyFormContainer, "autoRefresh", oData);
+			this.addInputField(oPropertyFormContainer, "mandante", oData);
+
+			oFormContainer = oPropertyFormContainer;
 		},
-		
-		getDefaultPropertyData: function(){
+
+		getDefaultPropertyData: function () {
 			return {
-				
-                "title": "zpluginPutBatchWCTA",
-				"text": "zpluginPutBatchWCTA"
-                
+
+				"autoRefresh": 15,
+				"mandante": "100"
+
 			};
 		}
 
