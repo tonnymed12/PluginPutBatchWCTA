59ba19d8e76c328a9d620e40ba809f0e6876b343
Anthony Medina <anthony.akmp99@gmail.com>
2026-05-26 15:38:48 +0000

    Se agrego la funcion que genera la resumen de orden para evitar que se pierda el contexto
    -Se redujo el tiempo del escaneo para ejecutar validacion

--- zpluginPutBatchWCTA/webapp/zpluginPutBatchWCTA/controller/MainView.controller.js
+++ zpluginPutBatchWCTA/webapp/zpluginPutBatchWCTA/controller/MainView.controller.js
@@ -754,7 +754,7 @@ sap.ui.define([
             this._oScanDebounceTimer = setTimeout(function () {
                 this._oScanDebounceTimer = null;
                 this.onBarcodeSubmit();
-            }.bind(this), 400);
+            }.bind(this), 200);
         },
         /**
          * Elimina un lote de la tabla y recorre los posteriores hacia arriba.
@@ -1055,6 +1055,7 @@ sap.ui.define([
             }
             gOperationPhase = oData;
             this.onGetCustomValues();
+            this.setOrderSummary();
 
         },
 
